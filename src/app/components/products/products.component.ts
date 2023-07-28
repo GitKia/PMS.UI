7d9b88297793ab0220132da75074a584e06e873b
PMS.UI <kza91@hotmail.com>
2023-07-28 14:02:03 +0200

    ProductsAdded

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-products',
@@ -6,6 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products: Product[] = [];
+  products: Product[] = [
+    {
+      id: '1111',
+      name: 'T-shirt',
+      type: 'Garments',
+      color: 'blue',
+      price: 9.99
+    },
+    {
+      id: '2222',
+      name: 'Samsung',
+      type: 'Mobile',
+      color: 'Black',
+      price: 1500
+    }
+  ];
 
 }
