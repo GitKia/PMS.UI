a3fb0643a5fc660084704811db1fd04db00069b1
PMS.UI <kza91@hotmail.com>
2023-07-28 15:45:11 +0200

    50%

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   products: Product[] = [];
 
+  constructor(private producsService: ProductsService, private router: Router){}
   
+  ngOnInit(): void {
+    this.producsService.getAllProducts()
+    .subscribe({
+      next: (products) => {
+        this.products = products;
+      }, 
+      error: (response) => {
+        console.log(response)
+      }
+    })
+  }
+
 }
