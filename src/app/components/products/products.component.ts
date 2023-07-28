f240badf9591397fbfd6b04beb80e0e38047c4ac
PMS.UI <kza91@hotmail.com>
2023-07-28 15:05:00 +0200

    FunctionsAdded

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -7,21 +7,7 @@ import { Product } from 'src/app/models/product.model';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products: Product[] = [
-    //{
-    //   id: '1111',
-    //   name: 'T-shirt',
-    //   type: 'Garments',
-    //   color: 'blue',
-    //   price: 9.99
-    // },
-    // {
-    //   id: '2222',
-    //   name: 'Samsung',
-    //   type: 'Mobile',
-    //   color: 'Black',
-    //   price: 1500
-    // }
-  ];
+  products: Product[] = [];
 
+  
 }
